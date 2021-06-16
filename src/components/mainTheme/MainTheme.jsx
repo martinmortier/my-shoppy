a347c331949a765292700fae3403b3fa06152d93
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-06-16 21:09:02 +0200

    Add react router + config

--- src/components/mainTheme/MainTheme.jsx
+++ src/components/mainTheme/MainTheme.jsx
@@ -1,15 +1,35 @@
 import { Header, Content } from "./MainTheme.styles"
-
+import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ShoppingCart from "../shoppingCart/ShoppingCart"
+import App from "../../pages/App"
 const MainTheme = (props) => {
     return (
-        <>
+        <div>
+            <Router>
             <Header>
-            NAVBAR HERE
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/shoppingCart">List of products</Link>
+                            </li>
+                    </ul>
+                    </nav>
             </Header>
             <Content>
-                { props.children }
+                <Switch>
+                    <Route path="/shoppingCart">
+                        <ShoppingCart />
+                    </Route>
+                    <Route path="/">
+                        <App />
+                    </Route>
+                </Switch>
             </Content>
-        </>
+            </Router>
+        </div>
     )
 }
 
