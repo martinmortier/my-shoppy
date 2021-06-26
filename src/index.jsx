c19d3f7cb752b7defebdebf2b2c1e9efa544258b
Mortier Martin <34869851+martinmortier@users.noreply.github.com>
2021-06-26 20:27:09 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,37 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import './index.css';
-import App from './pages/App'
-import reportWebVitals from './reportWebVitals';
-import store from './redux/store'
-import MainTheme from './components/mainTheme/MainTheme'
-import { Switch, Route } from 'react-router-dom'
-import ShoppingCart from "./components/shoppingCart/ShoppingCart"
-import Login from './components/login/Login'
-import ProductList from './components/productList/ProductList';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./pages/App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+import MainTheme from "./components/mainTheme/MainTheme";
+import { Switch, Route } from "react-router-dom";
+import ShoppingCart from "./components/shoppingCart/ShoppingCart";
+import Login from "./components/login/Login";
+import ProductList from "./components/productList/ProductList";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <MainTheme>
         <Switch>
           <Route path="/shoppingCart">
-              <ShoppingCart />
+            <ShoppingCart />
           </Route>
           <Route path="/login">
-              <Login />
+            <Login />
           </Route>
           <Route path="/productList">
             <ProductList />
           </Route>
           <Route path="/">
-              <App />
+            <App />
           </Route>
         </Switch>
       </MainTheme>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
